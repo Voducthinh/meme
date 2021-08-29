ead87bb68d0ddd0f651ae97b47175bf159d5d7aa
Trần Trọng Mạnh <89680872+ttm2004@users.noreply.github.com>
2021-08-30 01:15:07 +0700

    Update adbot.js

--- modules/commands/adbot.js
+++ modules/commands/adbot.js
@@ -17,17 +17,17 @@ module.exports.config = {
 module.exports.run = async({api,event,args,Users,global,Currencies}) => {
 var callback = () => api.sendMessage(
   {body:`梁Admin Bot梁\n
-👀 Tên: Nguyễn Hoàng Anh ( Jutsu )
-❎ Tuổi: 19
+👀 Tên: Trần Trọng Mạnh
+❎ Tuổi: 17
 👤 😶 Giới tính: Nam
-💫 Chiều cao cân nặng: 1m78 51 kg
+💫 Chiều cao cân nặng: 1m65 45 kg
 💘 Mối quan hệ: Độc thân
-😎 Quê quán: Vinh
-👫 Gu: Dú to là được :))
+😎 Quê quán: Nghệ An 
+👫 Gu: Thích mình là được ))
 🌸 Tính cách: Bên ngoài lạnh lùng boiz kiểu Hàn Quốc bên trong ít nói
-🌐 Facebook: Https://www.facebook.com/Admin.HoangAnh.Booking
-🌀 Sở thích: Chơi game, xem phim anime blabla, ăn, ngủ
-👉 Contact: Https://NguyenHoangAnhProCoder.github.io/Profile`,
+🌐 Facebook: Https://www.facebook.com/ttm130
+🌀 Sở thích: Chơi game, lập trình blabla, ăn, ngủ
+👉 Contact: Https://trantrongmanh2004@gmail.com`,
     attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(
